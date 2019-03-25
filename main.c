c411e0c8fb44d2710b90a5dd131ae847ba3c1add
angristan <angristan@pm.me>
2019-03-25 22:23:31 +0100

    Cleanup and format code

--- main.c
+++ main.c
@@ -2,51 +2,39 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 #include <glib/gprintf.h>
-#include "lib.h"
+#include "functions.h"
 
-
-
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
     GtkWidget *mainWindow = NULL;
     GtkBuilder *builder = NULL;
     GtkButton *validateButton = NULL;
     GtkButton *showCart = NULL;
-
     GError *error = NULL;
 
     gtk_init(&argc, &argv);
 
     builder = gtk_builder_new();
-    gtk_builder_add_from_file(builder, "../GladeFFW.glade", &error);
+    gtk_builder_add_from_file(builder, "./GladeFFW.glade", &error);
 
-
-
-    if (error){
+    if (error)
+    {
         gint code = error->code;
-        g_printerr("%s\n", error ->message);
+        g_printerr("%s\n", error->message);
         g_error_free(error);
         return code;
     }
 
     gtk_builder_connect_signals(builder, NULL);
 
-    mainWindow = GTK_WIDGET(gtk_builder_get_object(builder,"homeWindow"));
-
+    mainWindow = GTK_WIDGET(gtk_builder_get_object(builder, "homeWindow"));
     validateButton = GTK_BUTTON(gtk_builder_get_object(builder, "validateButton"));
-
     showCart = GTK_BUTTON(gtk_builder_get_object(builder, "cartButton"));
 
-
-
     g_signal_connect(G_OBJECT(mainWindow), "destroy", (GCallback)OnDestroy, NULL);
-
-    g_signal_connect(G_OBJECT(validateButton), "clicked", (GCallback) OpenScan, builder);
-
-    g_signal_connect(G_OBJECT(showCart), "clicked", (GCallback) OpenCart, builder);
-
-
+    g_signal_connect(G_OBJECT(validateButton), "clicked", (GCallback)OpenScan, builder);
+    g_signal_connect(G_OBJECT(showCart), "clicked", (GCallback)OpenCart, builder);
 
     gtk_widget_show_all(mainWindow);
 
